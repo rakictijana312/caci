7ec7ea3064245be199820ba60a9218c0e148ebaf
rakic <rakicijana312@gmail.com>
2021-11-26 11:47:03 -0500

    practicing loops

---
+++ src/MyREVIEW/CACI/DoWhileLoop.java
@@ -0,0 +1,33 @@
+package MyREVIEW.CACI;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        //https://courses.caveofprogramming.com/courses/2081/lectures/38454
+
+     /*   Scanner scanner=new Scanner(System.in);
+        System.out.println("Scanner scanner=new Scanner(System.in); ");
+        int value= scanner.nextInt();
+        while(value !=5){
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        System.out.println("Good job!"); */
+
+
+        //DO WHILE loop will first print and them check conditions
+        Scanner scanner = new Scanner(System.in);
+        int value = 0;
+        do {
+            System.out.println("Enter a number : ");//ENTER VALE
+            value= scanner.nextInt();//CAPTURE VALUE
+
+        }
+        while(value !=5);
+        System.out.println("Great JOB!");
+
+
+    }
+}
--- src/MyREVIEW/CACI/HTMLTables.java
+++ src/MyREVIEW/CACI/HTMLTables.java
@@ -1,4 +1,4 @@
-package MyREVIEW;
+package MyREVIEW.CACI;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
