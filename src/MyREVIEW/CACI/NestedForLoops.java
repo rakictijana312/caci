0a14969d6a041244be9f662b711439ce5e06d130
rakic <rakicijana312@gmail.com>
2021-11-26 12:16:32 -0500

    Nested for loops

--- src/MyREVIEW/CACI/NestedForLoops.java
+++ src/MyREVIEW/CACI/NestedForLoops.java
@@ -35,7 +35,7 @@ public class NestedForLoops {
 
         System.out.println("----------");
 
-        //Third way
+        //Third way of
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 10; j++) {
                 System.out.println("i: " + i + ", j: " + j);
